091e5bc25ac8e2792cf5f71a0f88dbb80c419c69
AKUnite2430 <77609837+AKUnite2430@users.noreply.github.com>
2021-01-18 13:45:19 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600542b4ae0c05001e8791fc",
+				"600542b4ae0c05001e8791fe"
+			]
+		},
+		"600542b4ae0c05001e8791fc": {
+			"id": "600542b4ae0c05001e8791fc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600542b4ae0c05001e8791fe": {
+			"id": "600542b4ae0c05001e8791fe",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
